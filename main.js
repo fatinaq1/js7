8cc73d813b406359f896ec77b70ba6a721d7ed67
fatinaq1 <146374275+fatinaq1@users.noreply.github.com>
2024-02-16 16:57:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function increase (){
+
+    let button = this;
+    let countContainer = button.closest(".part");
+    let counter = countContainer.querySelector(".counter").textContent;
+    let total = parseInt(document.querySelector(".total").textContent);
+    total += 1 ;
+    let to =total.toString();
+    
+    let count1 = parseInt(counter);
+    count1 += 1 ;
+    let st =count1.toString();
+    countContainer.querySelector(".counter").textContent = st;
+    document.querySelector(".total").textContent = to;
+    
+ }
+
+ function reset (){
+let all = document.querySelectorAll(".part span");
+let total = document.querySelector(".total");
+
+for (let i = 0; i < all.length; i++) {
+    all[i].textContent = 0;
+}
+
+total.textContent = 0;
+
+ }
